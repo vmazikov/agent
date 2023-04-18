fb47d2bc495403691b2a164e5234b5eb31f37b93
Vladislav Mazikov <vladislavmazikov@yandex.ru>
2023-04-18 20:25:55 +0700

    Правки в адреске

--- scripts/ras.js
+++ scripts/ras.js
@@ -6,16 +6,18 @@ const buttonTopki = document.querySelector('#topki');
 const buttonPromislennaya = document.querySelector('#promislennaya');
 const buttonBerezovskiy = document.querySelector('#berezovskiy');
 const buttonObKem = document.querySelector('#ob_kem');
+const buttonBel = document.querySelector('#bel');
 
 
 const kemIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3Ac78f6249ae901c61481365556adf332ebe6638afdb62893dccc471dedf6f1e1f&amp;source=constructor';
 const anjIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3Ae383ca5ae63ef9bf7a647c9522c03191575ff6bf18116f19ddc56f9d24e2799d&amp;source=constructor';
 const prokopIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A82093b17f6f7895efffc35c532f90b8d3a4d2c70f01b5c77da2b7dd05aa63001&amp;source=constructor';
-const mapIframe ='https://yandex.ru/map-widget/v1/?um=constructor%3Aed277b9e21482d6154077beb8c0d0da0711a85494c66e4cd71a514508165316d&amp;source=constructor'
-const topkiIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3Ad327bd1aca3c480a66db69a155fdd03f50acbd9be96be3c75feb9910e0302e06&amp;source=constructor'
-const promislennayaIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A0b26db91c0bfc41774b87f41d83f3b36a02a38eeb00c0a3141ce8a8e8471d92e&amp;source=constructor'
-const berezovskiyIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A6d8ad1d0e8ed2b5587acd265f449dfdf8e2856815c78e1d0a684b9475b3dded4&amp;source=constructor'
-const obKemIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A259785c63be2612b3e8dfe7c7cd088094f4ec32e5d0535ddcb90d4c09f509c0d&amp;source=constructor'
+const mapIframe ='https://yandex.ru/map-widget/v1/?um=constructor%3Aed277b9e21482d6154077beb8c0d0da0711a85494c66e4cd71a514508165316d&amp;source=constructor';
+const topkiIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3Ad327bd1aca3c480a66db69a155fdd03f50acbd9be96be3c75feb9910e0302e06&amp;source=constructor';
+const promislennayaIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A0b26db91c0bfc41774b87f41d83f3b36a02a38eeb00c0a3141ce8a8e8471d92e&amp;source=constructor';
+const berezovskiyIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A6d8ad1d0e8ed2b5587acd265f449dfdf8e2856815c78e1d0a684b9475b3dded4&amp;source=constructor';
+const obKemIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3A259785c63be2612b3e8dfe7c7cd088094f4ec32e5d0535ddcb90d4c09f509c0d&amp;source=constructor';
+const belIframe = 'https://yandex.ru/map-widget/v1/?um=constructor%3Abaf7cea972c0f9e599bd7a26fc84eb7d21a6e488a8983341213acc42702f8161&amp;source=constructor';
 const iframe = document.querySelector('.iframe__item');
 
 buttonKem.addEventListener('click', function(){
@@ -56,6 +58,11 @@ buttonBerezovskiy.addEventListener('click', function(){
 buttonObKem.addEventListener('click', function(){
   iframe.src = '';
   iframe.src = obKemIframe;
+})
+
+buttonBel.addEventListener('click', function(){
+  iframe.src = '';
+  iframe.src = belIframe;
 })
 
 // buttonBel.addEventListener('click', function(){
